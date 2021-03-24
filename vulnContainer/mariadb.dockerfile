ef13d065ed738a6452d20280cef2e0d652c3803c
unknown <sam.plant998@gmail.com>
2021-03-24 17:13:23 +0000

    24/03/21

--- vulnContainer/mariadb.dockerfile
+++ vulnContainer/mariadb.dockerfile
@@ -2,5 +2,6 @@ FROM mariadb
 
 ENV MYSQL_ROOT_PASSWORD tiaspbiqe2r
 ENV MYSQL_DATABASE university
+RUN
 
 COPY ./uni.sql /docker-entrypoint-initdb.d/
--- vulnContainer/uni.sql
+++ vulnContainer/uni.sql
@@ -26,7 +26,7 @@ SET time_zone = "+00:00";
 --
 -- Table structure for table `course`
 --
-USE university;
+CREATE DATABASE university;
 
 CREATE TABLE `course` (
   `id` int(11) NOT NULL,
